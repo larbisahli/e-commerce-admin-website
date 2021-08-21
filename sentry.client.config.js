d127db5c76d0dcdbe9ac2c256fa5e1acafb5c93a
Larbi Sahli <larbisahli1905@gmail.com>
2021-08-21 23:22:39 +0100

    husky remove prettier

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-const ProductionEnv = process.env.NODE_ENV === 'production'
+const ProductionEnv = process.env.NODE_ENV === 'production';
 
 Sentry.init({
   dsn: ProductionEnv ? SENTRY_DSN : '',
