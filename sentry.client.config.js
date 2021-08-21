42219dceb0af4c97561995d69e832e74d69cf006
Larbi Sahli <larbisahli1905@gmail.com>
2021-08-21 23:56:38 +0100

    sentry fix dns

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,10 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-const ProductionEnv = process.env.NODE_ENV === 'production';
 
 Sentry.init({
-  dsn: ProductionEnv ? SENTRY_DSN : '',
+  dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
   debug: false,
   ignoreErrors: ['ResizeObserver loop limit exceeded'],
